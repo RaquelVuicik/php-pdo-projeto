4c99280320f4a3409cc69f89cfb62678b43dd49d
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-10-31 11:53:14 -0300

    prepared statements

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -7,8 +7,18 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$student = new Student(null, 'Raquel Vuicik', new \DateTimeImmutable('1994-11-21'));
+$student = new Student(
+    null,
+    "Raquel', ''); DROP TABLE students; -- Vuicik",
+    new \DateTimeImmutable('1994-11-21')
+);
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}')";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(1, $student->name());
+$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
+$statement->execute();
 
-var_dump($pdo->exec($sqlInsert));
+if ($statement->execute()) {
+    echo "Aluno incluído";
+}
